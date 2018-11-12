a5c1ba3cdc75dcb4aa35a29dd3f0b06b6e9abcde
devasish <dev.achieve@gmail.com>
2018-11-12 10:10:05 +0530

    initial

---
+++ application/views/main/main.php
@@ -0,0 +1,20 @@
+<?php
+
+$this->load->view('main/header.php');
+
+?>
+
+<div class="wrapper ">
+<?php 
+
+$this->load->view('main/sidebar.php');
+$this->load->view('main/main-panel.php');
+
+?>
+</div>
+
+
+
+<?php
+
+$this->load->view('main/footer.php');
